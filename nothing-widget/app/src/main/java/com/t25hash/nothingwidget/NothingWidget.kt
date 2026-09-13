8d55e062208d4b5a2e0fd94f7185d01005c4afc5
t25-hash <t2521018@gmail.com>
2026-09-13 17:24:52 +0900

    Fix build: Alignment lives in androidx.glance.layout, not androidx.glance

--- nothing-widget/app/src/main/java/com/t25hash/nothingwidget/NothingWidget.kt
+++ nothing-widget/app/src/main/java/com/t25hash/nothingwidget/NothingWidget.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
-import androidx.glance.Alignment
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.action.actionStartActivity
@@ -13,6 +12,7 @@ import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.text.FontWeight
